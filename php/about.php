5d56a64016eac488db6782ff1f88dbd5fda7f5ae
Jon Prentice <jon77p@gmail.com>
2017-03-07 19:58:10 +0000

    Added fadeIn effect

--- php/about.php
+++ php/about.php
@@ -1,6 +1,6 @@
 <?php $title = 'About'; include("template/header.php");?>
 <h1>About</h1>
-<h2>musician | arranger</h2>
-<h2>hardware and systems management | programmer</h2>
+<h2><span class="fadeIn">musician</span> | <span class="fadeIn">arranger</span></h2>
+<h2><span class="fadeIn">hardware and systems management</span> | <span class="fadeIn">programmer</span></h2>
 
 <?php include("template/footer.php");?>
