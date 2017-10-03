054e104aa04fb5042ca15fe2ccda3b49eae14a46
Jon Prentice <jon77p@gmail.com>
2017-10-03 02:07:22 +0000

    Added Kit social button and fixed formatting of about.php code

--- php/about.php
+++ php/about.php
@@ -2,11 +2,11 @@
 <h1 class="text-center">About</h1>
 
 <div class="row-fluid">
-<div class="col-md-offset-2 text-center">
-<h3 class="row"><span class="fadeIn col-md-4"><a href="" target="__blank">musician</a></span> <span class="col-md-2"> |</span> <span class="fadeIn col-md-4"><a href="" target="__blank">arranger</a></span></h3>
-<h3 class="row"><span class="fadeIn col-md-4"><a href="http://photos.jonprentice.me" target="__blank">photographer</a></span> <span class="col-md-2"> |</span> <span class="fadeIn col-md-4"><a href="" target="__blank">programmer</a></span></h3>
-<h3 class="row"><span class="fadeIn col-md-4">hardware design</span> <span class="col-md-2"> |</span> <span class="fadeIn col-md-4">OS design</span></h3>
-</div>
+	<div class="col-md-offset-2 text-center">
+		<h3 class="row"><span class="fadeIn col-md-4"><a href="" target="__blank">musician</a></span> <span class="col-md-2"> |</span> <span class="fadeIn col-md-4"><a href="" target="__blank">arranger</a></span></h3>
+		<h3 class="row"><span class="fadeIn col-md-4"><a href="http://photos.jonprentice.me" target="__blank">photographer</a></span> <span class="col-md-2"> |</span> <span class="fadeIn col-md-4"><a href="" target="__blank">programmer</a></span></h3>
+		<h3 class="row"><span class="fadeIn col-md-4">hardware design</span> <span class="col-md-2"> |</span> <span class="fadeIn col-md-4">OS design</span></h3>
+	</div>
 </div>
 
 <div class="row-fluid">
@@ -21,57 +21,62 @@
   </div>
 </div>
 
+<div class="container-fluid">
 <div class="row-fluid">
   <div class="text-center"><h2 class="heading">Social Media</h2></div>
     <div class="col-md-offset-0 social-buttons">
-	  <div class="row">
+	  <div class="row-fluid">
 	    <div class="col-md-3" style="display: inline;">
-		  <a class="fadeIn btn btn-block btn-social btn-facebook" href="https://facebook.com/jon77p" target="__blank">
-		    <i class="fa fa-facebook"></i> Facebook
-   		  </a>
-		  <a class="fadeIn btn btn-block btn-social btn-twitter" href="https://twitter.com/jon77p" target="__blank">
-		    <i class="fa fa-twitter"></i> Twitter
-	 	  </a>
-		  <a class="fadeIn btn btn-block btn-social btn-instagram" href="https://instagram.com/jon77p" target="__blank">
-		    <i class="fa fa-instagram"></i> Instagram
-		  </a>
+			  <a class="fadeIn btn btn-block btn-social btn-facebook" href="https://facebook.com/jon77p" target="__blank">
+			    <i class="fa fa-facebook"></i> Facebook
+	   		  </a>
+			  <a class="fadeIn btn btn-block btn-social btn-twitter" href="https://twitter.com/jon77p" target="__blank">
+			    <i class="fa fa-twitter"></i> Twitter
+		 	  </a>
+			  <a class="fadeIn btn btn-block btn-social btn-instagram" href="https://instagram.com/jon77p" target="__blank">
+			    <i class="fa fa-instagram"></i> Instagram
+			  </a>
 	    </div>
-	  <div class="col-md-3" style="display: inline;">
-		<a class="fadeIn btn btn-block btn-social btn-snapchat disabled">
-		  <i class="fa fa-snapchat-ghost"></i> Snapchat
-		</a>
-		<a class="fadeIn btn btn-block btn-social btn-reddit" href="https://reddit.com/u/jon77p" target="__blank">
-		  <i class="fa fa-reddit"></i> Reddit
-		</a>
-		<a class="fadeIn btn btn-block btn-social btn-google" href="https://plus.google.com/u/0/+JonPrentice1" target="__blank">
-		  <i class="fa fa-google-plus"></i> Google+
-		</a>
-	  </div>
-	  <div class="col-md-3" style="display: inline;">
-		<a class="fadeIn btn btn-block btn-social btn-linkedin disabled">
-		  <i class="fa fa-linkedin"></i> LinkedIn
-		</a>
-		<a class="fadeIn btn btn-block btn-social btn-spotify" href="https://open.spotify.com/user/jon77p" target="__blank">
-		  <i class="fa fa-spotify"></i> Spotify
-		</a>
-		<a class="fadeIn btn btn-block btn-social btn-soundcloud" href="https://soundcloud.com/jon77p" target="__blank">
-		  <i class="fa fa-soundcloud"></i> SoundCloud
-		</a>
-	  </div>
-	  <div class="col-md-3" style="display: inline;">
-		<a class="fadeIn btn btn-block btn-social btn-musescore" href="https://musescore.com/jon77p" target="__blank">
-		  <i class="fa fa-music"></i> MuseScore
-		</a>
-		<a class="fadeIn btn btn-block btn-social btn-github" href="https://github.com/jon77p" target="__blank">
-		  <i class="fa fa-github"></i> GitHub
-		</a>
-		<a class="fadeIn btn btn-block btn-social btn-">
-		  <i class="fa fa-"></i> _
-		</a>
+		  <div class="col-md-3" style="display: inline;">
+				<a class="fadeIn btn btn-block btn-social btn-snapchat disabled">
+				  <i class="fa fa-snapchat-ghost"></i> Snapchat
+				</a>
+				<a class="fadeIn btn btn-block btn-social btn-reddit" href="https://reddit.com/u/jon77p" target="__blank">
+				  <i class="fa fa-reddit"></i> Reddit
+				</a>
+				<a class="fadeIn btn btn-block btn-social btn-google" href="https://plus.google.com/u/0/+JonPrentice1" target="__blank">
+				  <i class="fa fa-google-plus"></i> Google+
+				</a>
+		  </div>
+		  <div class="col-md-3" style="display: inline;">
+				<a class="fadeIn btn btn-block btn-social btn-linkedin disabled">
+				  <i class="fa fa-linkedin"></i> LinkedIn
+				</a>
+				<a class="fadeIn btn btn-block btn-social btn-spotify" href="https://open.spotify.com/user/jon77p" target="__blank">
+				  <i class="fa fa-spotify"></i> Spotify
+				</a>
+				<a class="fadeIn btn btn-block btn-social btn-soundcloud" href="https://soundcloud.com/jon77p" target="__blank">
+				  <i class="fa fa-soundcloud"></i> SoundCloud
+				</a>
+		  </div>
+		  <div class="col-md-3" style="display: inline;">
+		  	<div class="container-fluid">
+					<a class="fadeIn btn btn-block btn-social btn-musescore" href="https://musescore.com/jon77p" target="__blank">
+					  <i class="fa fa-music"></i> MuseScore
+					</a>
+					<a class="fadeIn btn btn-block btn-social btn-github" href="https://github.com/jon77p" target="__blank">
+					  <i class="fa fa-github"></i> GitHub
+					</a>
+					<a class="fadeIn btn btn-block btn-social btn-kit" href="https://kit.com/jon77p" target="__blank">
+					  <i class="fa fa-columns"></i> Kit
+					</a>
+				</div>
+		  </div>
 	  </div>
 	</div>
-  </div>
-  <div class="text-center"><h2>Need support? <a href="mailto:jon77p+support@gmail.com">Contact me</a>.</h2></div>
+<div class="text-center"><h2>Need support? <a href="mailto:jon77p+support@gmail.com">Contact me</a>.</h2></div>
+
+</div>
 </div>
 
 <?php include("template/footer.php");?>
