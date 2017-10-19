a4b81ba1a2672816a6667c61e2891c7486a20867
Jon Prentice <jon77p@gmail.com>
2017-10-19 16:19:33 +0000

    Created help.php page

---
+++ php/help.php
@@ -0,0 +1,18 @@
+<?php $title = 'jonprentice'; $active = 'active'; include("template/header.php");?>
+
+<div class="container">
+	<div class="text-center">
+		<h1>Help</h1>
+
+		<h2>Have a <b><em>Question</em></b> or <b><em>Issue</em></b> that you need help with?</h2>
+		<h3>Email me at <a href="mailto:jon77p_support@gmail.com">jon77p_support@gmail.com</a>.</h3>
+		<h4>Please make your subject header be <b>[SUPPORT]</b>.</h4>
+
+		<br>
+
+		<h2>Want to request <b><em>Remote Support</em></b>?</h2>
+		<h3>Visit <a href="https://get.teamviewer.com/v7nxbyc" target="__blank">https://get.teamviewer.com/v7nxbyc</a> to add yourself to the remote service queue and I will remotely help you. </h3>
+	</div>
+</div>
+
+<?php include("template/footer.php");?>
